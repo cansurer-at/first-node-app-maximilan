2f061c6bb000cd36412414924504c37a6b5216b8
Can Surer <cansurer@outlook.com>
2025-01-05 17:28:49 +0100

    pug in a nutshell

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 
-app.set("view", "pug");
+app.set("view engine", "pug");
+app.set("views", "views");
 
-const adminData = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404-not-found.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
