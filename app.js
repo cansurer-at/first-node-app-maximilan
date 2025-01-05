f4b854e12520a6e67596d249cbfa8607c4159d4d
Can Surer <cansurer@outlook.com>
2025-01-05 19:33:13 +0100

    mvc model create controller

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
