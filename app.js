98cb6c21aa05e8403e4ca0b8218771a3432b0805
Can Surer <cansurer@outlook.com>
2025-01-05 12:29:05 +0100

    feat: using express router

--- app.js
+++ app.js
@@ -3,27 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//order matters
-
-app.use("/add-product", (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">Add product</button>
-    </form>
-  `);
-});
-
-//filtering  request 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
