392af6ce08bf2a2ea901d0c1da4d11aa05f4b518
Can Surer <cansurer@outlook.com>
2025-01-07 13:56:23 +0100

    storing data in files via the model

--- models/product.js
+++ models/product.js
@@ -1,3 +1,7 @@
+//saving the input as a file without db connection
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
@@ -5,10 +9,45 @@ module.exports = class Product {
     this.title = t;
   }
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        //JSON.parse takes JSON and converts into object or array
+
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      //JSON.stringify takes javascript object or array converts into JSON
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
     return products;
   }
 };
+
+//Saving just into storage without creating a file with it
+
+// const products = [];
+
+// module.exports = class Product {
+//   constructor(t) {
+//     this.title = t;
+//   }
+//   save() {
+//     products.push(this);
+//   }
+
+//   static fetchAll() {
+//     return products;
+//   }
+// };
